85f5df96b6de0113a0ee3919af0f5ff76543f09a
brusnitsyn <andrew.brusnitsyn@gmail.com>
2022-12-09 01:40:16 +0900

    [st] change cfg urls

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,12 +1,12 @@
 export default {
   publicRuntimeConfig: {
-    serverURL: process.env.NODE_ENV === 'production' ? process.env.SERVER_URL : 'http://192.168.1.109:8000',
-    serverAPI: process.env.NODE_ENV === 'production' ? process.env.SERVER_API : 'http://192.168.1.109:8000/api',
+    serverURL: process.env.NODE_ENV === 'production' ? process.env.SERVER_URL : 'http://127.0.0.1:8000',
+    serverAPI: process.env.NODE_ENV === 'production' ? process.env.SERVER_API : 'http://127.0.0.1:8000/api',
   },
 
-  server: {
-    host: '0.0.0.0'
-  },
+  // server: {
+  //   host: '0.0.0.0'
+  // },
 
   loading: {
     color: '#1f2937',
@@ -94,7 +94,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://192.168.1.109:8000/api',
+    baseURL: process.env.SERVER_API,
     credentials: true
   },
 
@@ -102,7 +102,7 @@ export default {
     strategies: {
       'laravelSanctum': {
         provider: 'laravel/sanctum',
-        url: 'http://192.168.1.109:8000',
+        url: process.env.SERVER_URL,
         endpoints: {
           login: {url: '/api/auth/login', method: 'post'},
           logout: {url: '/api/auth/logout', method: 'post'},
