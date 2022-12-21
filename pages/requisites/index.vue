7ad9b578abd1823499a7f2c8a6aea05d6531c130
brusnitsyn <andrew.brusnitsyn@gmail.com>
2022-12-21 23:52:12 +0900

    [new]: requisites page

---
+++ pages/requisites/index.vue
@@ -0,0 +1,31 @@
+<script>
+
+export default {
+  layout: 'page',
+  data() {
+    return {
+      document: null
+    }
+  },
+  async fetch() {
+    this.document = await this.$content("requisites").fetch();
+  }
+}
+</script>
+
+<template>
+  <PageWrapper>
+    <PageBody>
+      <PageHeader>
+        <PageTitle text="Реквизиты" />
+      </PageHeader>
+      <PageSection>
+        <NuxtContent :document="document" />
+      </PageSection>
+    </PageBody>
+  </PageWrapper>
+</template>
+
+<style scoped>
+
+</style>
