d225e8a8659405ae9def14609d118fa1586935d0
brusnitsyn <andrew.brusnitsyn@gmail.com>
2023-01-10 01:20:05 +0900

    [add]: new style

--- components/Button.vue
+++ components/Button.vue
@@ -32,20 +32,22 @@ export default {
   },
   data() {
     return {
-      defaultStyle: 'border transition-color duration-300 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:dark:ring-offset-gray-50 focus:dark:ring-gray-400 focus:ring-gray-600/60 focus:ring-offset-gray-800/60 flex items-center justify-center gap-x-2 font-semibold',
+      defaultStyle: 'border transition-color duration-300 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:dark:ring-offset-gray-50 focus:dark:ring-gray-400 focus:ring-gray-600/60 focus:ring-offset-gray-800/60 flex items-center justify-center gap-x-2 ',
       styles: {
         none: 'cursor-pointer bg-transparent',
         default: 'cursor-pointer',
+        bordered: 'cursor-pointer text-white border border-2 border-white hover:border-primary-400',
         primary: 'cursor-pointer text-white bg-primary-500 hover:bg-primary-400 border-primary-500',
         secondary: 'cursor-pointer text-slate-800 bg-gray-200 border-gray-200 hover:bg-gray-300 dark:text-white dark:border-slate-800 dark:bg-slate-800 dark:hover:bg-slate-700',
         opposite: 'cursor-pointer text-white bg-gray-800 hover:bg-white hover:text-gray-800 hover:border-gray-900 dark:text-gray-800 dark:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-100 dark:border-white',
         disabled: 'text-gray-500 bg-gray-100 dark:text-white dark:border-slate-800 dark:bg-slate-800'
       },
       sizes: {
-        lg: 'h-13 px-8 text-lg rounded-lg',
+        lg: 'h-12 px-8 text-lg rounded-lg',
         md: 'h-10 px-6 text-base rounded',
         sm: 'h-9 px-4 text-sm rounded',
-        xs: 'h-6 px-3 text-xs rounded'
+        xs: 'h-6 px-3 text-xs rounded',
+        rounded: 'h-12 px-8 text-sm rounded-full font-medium',
       },
     }
   },
